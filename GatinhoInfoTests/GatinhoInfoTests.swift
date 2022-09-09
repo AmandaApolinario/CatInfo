3a6be83400f2c5797c4b44187c137a4a72eab910
amanda <amandabrito84@gmail.com>
2022-09-09 09:04:01 -0300

    iniciando testes unitarios

---
+++ GatinhoInfoTests/GatinhoInfoTests.swift
@@ -0,0 +1,44 @@
+//
+//  GatinhoInfoTests.swift
+//  GatinhoInfoTests
+//
+//  Created by Inpeaceapp on 06/09/2022.
+//  Copyright © 2022 Amanda. All rights reserved.
+//
+
+import XCTest
+@testable import GatinhoInfo
+
+class GatinhoInfoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testNumeroDeItensDaTableView() {
+//        let viewModel = TableViewModel()
+//        let tableView = TableViewController()
+//        XCTAssertEqual(viewModel.catsInfo.count, tableView.viewModel.itemCount)
+
+        
+    }
+    
+}
