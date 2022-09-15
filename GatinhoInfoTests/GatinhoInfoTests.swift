34a99071cbc5b0572416bef1fa9ee11d2dc2b0c6
amanda <amandabrito84@gmail.com>
2022-09-15 10:03:02 -0300

    funcao tearDown

--- GatinhoInfoTests/GatinhoInfoTests.swift
+++ GatinhoInfoTests/GatinhoInfoTests.swift
@@ -23,7 +23,8 @@ class GatinhoInfoTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+        tableViewController = nil
         super.tearDown()
     }
     
